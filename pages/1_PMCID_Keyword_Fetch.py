446f6c8f663750fe72966d57df7439c5bed0fe41
eliasxlevy <elias.levy@vanderbilt.edu>
2025-02-25 17:09:06 -0600

    Clarified Instructions

--- pages/1_PMCID_Keyword_Fetch.py
+++ pages/1_PMCID_Keyword_Fetch.py
@@ -1,6 +1,40 @@
 import streamlit as st
 
-st.write("First you'll want to go to https://ncbi.nlm.nih.gov/pmc/")
-st.write("Then you want to search a topic in the following format: open access[filter] <topic>")
-st.write("After this, you can click 'Send to', 'file', 'Format: PMCID List', 'Create File'")
-st.write("Move that file to the root directory of this project, and continue to the next step.")
+
+def main():
+    st.title("PMCID Extraction")
+
+    # Step 1: Go to https://pmc.ncbi.nlm.nih.gov/
+    st.write("1. Go to https://pmc.ncbi.nlm.nih.gov/ and search for your topic")
+
+    # Step 2: Apply Filters
+    st.write("2. On the leftmost column of the website, select:")
+    st.markdown("- The 'Open Access' filter under article attributes")
+    st.markdown("- Optionally, limit how old the articles are under 'Publication Date'")
+
+    # Step 3: Export the results
+    st.write(
+        "3. Export the results by clicking 'Send to:' (located under the right side of the search bar)"
+    )
+    st.markdown("- Under 'Choose Destination', select 'File'")
+    st.markdown("- In the 'Format' dropdown, select 'PMCID List'")
+    st.markdown("- Click the 'Create File' button to download the file")
+    st.write(
+        "4. Move the downloaded file to the root directory of your project and rename (especially if you will create multiple chatbots)"
+    )
+    st.write("Proceed to the next step.")
+
+    st.markdown(
+        """
+    <style>
+    [data-testid="stMarkdownContainer"] ul{
+        list-style-position: inside;
+    }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
