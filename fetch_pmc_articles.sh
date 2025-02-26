3a24f0a3af79bd8e2b2d3f2c8a33fa6a4723ae97
eliasxlevy <elias.levy@vanderbilt.edu>
2025-02-26 16:49:13 -0600

    BioC article downloader works

--- fetch_pmc_articles.sh
+++ fetch_pmc_articles.sh
@@ -2,30 +2,39 @@
 
 start_time=$(date +%s)
 
+# Define configuration for using the PubMed Central Open Access (PMCOA) API
 base_url="https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi"
 format="BioC_json"
 encoding="ascii"
-
-output_dir="pmc_articles"
+input_file="$1"
+keyword=$(basename "$input_file" _pmc_result.txt)
+output_dir="fulltext_articles/${keyword}_pmc_articles"
 mkdir -p "$output_dir"
 
-total_count=$(wc -l < pmc_result.txt)
+# Get the total count of PMCIDs in the input file
+total_count=$(wc -l < "$input_file")
 current_count=0
 
+# Iterate through the PMCIDs in the input file
 while IFS= read -r pmcid
 do
     current_count=$((current_count + 1))
+
+    # Construct the API URL for the current PMCID
     url="${base_url}/${format}/${pmcid}/${encoding}"
+
+    # Use cURL to fetch the article in BioC JSON format and capture the HTTP response code
     response=$(curl -s -w "%{http_code}" "$url" -o "${output_dir}/${pmcid}.json")
     http_code=${response: -3}
-    
-    if [ $http_code -eq 200 ]; then
+
+    # Check the HTTP response code
+    if [ "$http_code" -eq 200 ]; then
         echo "[$current_count/$total_count] Successfully fetched article $pmcid"
     else
         echo "[$current_count/$total_count] Failed to fetch article $pmcid: HTTP $http_code"
         rm "${output_dir}/${pmcid}.json"
     fi
-done < pmc_result.txt
+done < "$input_file"
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
--- pages/2_BioC_Article_Downloader.py
+++ pages/2_BioC_Article_Downloader.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import subprocess
 
-st.write("Now, you have a file containing PMCIDs of papers related to your topic.")
-st.write("In this step, we use this file to retrieve the full text articles from PubMed Central in JSON format.")
-st.write("To do this, run the fetch_pmc_articles.py script which fetches papers at a rate of ~10k articles/hour.")
+def main():
+    st.title("BioC JSON Article Downloader")
+
+    uploaded_file = st.file_uploader("Choose an input file", type="txt")
+    if uploaded_file is not None:
+        input_file_path = f"pmcids/{uploaded_file.name}"
+
+        if st.button("Begin Downloading"):
+            try:
+                result = subprocess.run(["bash", "fetch_pmc_articles.sh", input_file_path], capture_output=True, text=True)
+                if result.returncode == 0:
+                    st.write(result.stdout)
+                else:
+                    st.error(f"An error occurred while processing the file: {result.stderr}")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
